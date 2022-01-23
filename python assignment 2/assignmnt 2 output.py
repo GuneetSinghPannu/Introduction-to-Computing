b6b673f6f87ced598766a429faac4f65b8817ed6
GuneetSinghPannu <97420159+GuneetSinghPannu@users.noreply.github.com>
2022-01-23 12:18:55 +0530

    Add files via upload

---
+++ python assignment 2/assignmnt 2 output.py
@@ -0,0 +1,55 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: C:/Users/Intel/Pictures/assignment 2.py ===============
+
+The solution of Question 1 is :
+
+(a)	The length of the given string is : 35
+(b)	The string in reverse would be : egaugnal evitisnes esac a si nohtyP
+(c)	The new string becomes : a case sensitive
+(d)	The replaced substring will be : Python is object oriented language
+(e)	The first occurence of the given substring "a" is at index no. = 10
+(f)	The inputted strings with no white spaces will be "Pythonisacasesensitivelanguage"
+
+
+The solution of Question 2 is :
+
+Enter your name : GUNEET SINGH PANNU
+Enter your SID : 21105052
+Enter the name of your department : ECE
+Enter your CGPA : 10
+
+Hey, GUNEET SINGH PANNU Here!
+My SID is 21105052
+I am from ECE department and my CGPA is 10.0
+
+
+The solution of Question 3 is :
+
+a.	a & b = 8
+b.	a | b = 58
+c.	a ^ b = 50
+d.	Left shift both a and b with 2 bits : a = 224, b = 40
+e.	Right shift a with 2 bits and b with 4 bits : a = 14, b = 0
+
+
+The solution of Question 4 is :
+
+Enter first number : 41
+Enter second number : 57
+Enter third number : 26
+The greatest number is 57
+
+
+The solution of Question 5 is :
+
+Enter the string : adfhakjfhanameeaihfsadjkfh
+Yes
+
+
+The solution of Question 6 is :
+
+Enter the dimensions to check whether triangle possible or not : 5 12 13
+Yes
+
