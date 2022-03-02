83ef4e44dc1c8d378bb46410f861c39296604eb1
GuneetSinghPannu <97420159+GuneetSinghPannu@users.noreply.github.com>
2022-03-02 11:30:40 +0530

    Add files via upload

---
+++ python assignment 4/python assignment 4 output.py
@@ -0,0 +1,81 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/Intel/AppData/Local/Programs/Python/Python310/python assignment 4 source.py
+ASSIGNMENT 4
+
+Q1
+SOL 1. :- Solution the problem of tower of Hanoi with three disks.
+
+Move Disk 1 from A to C
+Move Disk 2 from A to B
+Move Disk 1 from C to B
+Move Disk 3 from A to C
+Move Disk 1 from B to A
+Move Disk 2 from B to C
+Move Disk 1 from A to C
+
+Q2
+ 
+ 
+Enter the number of rows in Pascal's Triangle: 5
+
+Using recursions:
+
+        1   
+      1   1   
+    1   2   1   
+  1   3   3   1   
+1   4   6   4   1   
+
+Using loops:
+
+        1   
+      1   1   
+    1   2   1   
+  1   3   3   1   
+1   4   6   4   1   
+
+Q3
+ 
+ 
+Enter the first integer: 85
+Enter the first integer: 17
+Remainder is:  0
+Quotient is:  5
+One value is zero
+{9}
+{9, 5}
+{9, 10, 5}
+{9, 10, 5, 6}
+{5, 6, 9, 10, 11}
+{5, 6, 9, 10, 11}
+Immutable set:  frozenset({5, 6, 9, 10, 11})
+Largest value in the set:  11
+Hash value:  11
+
+Q4
+ 
+ 
+Object created
+The name of the student is Guneet Singh Pannu and SID is 21105052.
+Object destroyed
+
+Q5
+ 
+a. The updated salary of the employee Mehak is 70000
+b. Employee Viren record deleted
+ 
+Q6
+ 
+ 
+Enter the first word: team
+
+Enter a new meaningful word to test your friendship: meat
+
+Does the word makes sense?(y or n)
+y
+The friends pass the friendship test!!
+
+
+
